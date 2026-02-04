e257c8e5cab2f1fdf65f34fefd4689b709402446
zalansimiczz <simicz.zalan.zoltan@diak.szbi-pg.hu>
2026-02-04 11:32:10 +0100

    verification email design berakása, testreszabasa

--- projekt/TollUtdij 2025.12/utdijpro-partner-portal/app/Providers/AppServiceProvider.php
+++ projekt/TollUtdij 2025.12/utdijpro-partner-portal/app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,6 +15,12 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        
+        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+        return (new MailMessage)
+            ->subject('TollÚtdíj - Erősítsd meg az email címed!')
+            ->line('Ahhoz, hogy ellenőrizni tudjuk a regisztráció során megadott e-mail címed helyességét, kérjük, erősítsd meg az alábbi linken!')
+            ->action('E-mail cím megerősítése', $url)
+            ->line('Miért fontos ez? A megerősítésed segít nekünk megbizonyosodni arról, hogy a megadott e-mail címedet nem egy illetéktelen, harmadik személy adta meg. Amennyiben nem te adtad meg az email címet, akkor egyszerűen hagyd figyelmen kívül ezt az üzenetet!');
+    });
     }
 }
